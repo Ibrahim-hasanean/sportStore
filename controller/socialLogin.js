802578907c33512eb2dfc95461c9de7b01866073
Ibrahim-hasanean <imh19970408@gmail.com>
2020-10-16 23:02:13 +0300

    fix google login expires

--- controller/socialLogin.js
+++ controller/socialLogin.js
@@ -59,7 +59,7 @@ module.exports = {
       });
     if (ExistUser) {
       let userToken = jwt.sign({ userId: ExistUser._id }, process.env.JWT_KEY, {
-        expiresIn: "1h"
+         expiresIn:"168h"
       });
       return res.json({
         status: 200,
@@ -78,7 +78,7 @@ module.exports = {
     });
     console.log(newUser);
     let userToken = jwt.sign({ userId: newUser._id }, process.env.JWT_KEY, {
-      expiresIn: "1h"
+       expiresIn:"168h"
     });
     res.json({
       status: 200,

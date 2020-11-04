52f44a47a9822c7788d63598532af3275768ed9d
Ibrahim-hasanean <imh19970408@gmail.com>
2020-11-04 17:10:35 +0200

    make discount unrequired in orders item object

--- models/orders.js
+++ models/orders.js
@@ -4,7 +4,7 @@ const itemsSchema = new Schema({
         itemId:{type:mongoose.Types.ObjectId},
         quantity:{type:Number,required:true},
         price:{type:Number,required:true},
-        discount:{type:Number,required:true},
+        discount:{type:Number},
         season:{type:String},
         category:{type:String},
         type:{type:String},
